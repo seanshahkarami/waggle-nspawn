0f2b9dd8d896bf428ecd75315f10a5efe963530a
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-09-13 22:27:10 -0500

    install base packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-set -u
 set -e
+set -u
 
 basedir=$1
-shift
 
-if [ -z $basedir ]; then
-  echo "usage: $0 basedir"
-  exit 1
-fi
-
+# bootstrap base debian system
 debootstrap stable $basedir
 
+# enable autologin for root
 mkdir -p $basedir/etc/systemd/system/console-getty.service.d
 cat << EOF > $basedir/etc/systemd/system/console-getty.service.d/override.conf
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --noclear --autologin root --keep-baud console 115200,38400,9600 $TERM
+EOF
+
+# ensure waggle image packages are installed
+systemd-nspawn -D $basedir bash -s << EOF
+apt-get update && apt-get install -y \
+  git \
+  python3 \
+  python3-pip
 EOF
 
 # add waggle-init override and make /wagglerw
